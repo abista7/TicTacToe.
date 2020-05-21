1199aef21a2b19eafcefcc48e2f612b682efb830
Arjun <arjunbista428@gmail.com>
2020-05-20 19:55:10 -0700

    #finalDone

--- server.js
+++ server.js
@@ -2,17 +2,84 @@ const WebSocket = require('ws');
 const express = require('express')
 const app = express()
 const port = 5000
-
+ 
 const wss = new WebSocket.Server({ port: 4000 });
-
-let gameState = [];
-
+ 
+let gameState = ['', '', '', '', '', '', '', '', ''];
+const players = [];
+let playerTurn = 0;
+ 
+function checkWinner(gameState, playerSymbol){ 
+ if(
+   (gameState[0] === playerSymbol && gameState[1] === playerSymbol && gameState[2] === playerSymbol) ||
+   (gameState[0] === playerSymbol && gameState[3] === playerSymbol && gameState[6] === playerSymbol) ||
+   (gameState[3] === playerSymbol && gameState[4] === playerSymbol && gameState[5] === playerSymbol) ||
+   (gameState[1] === playerSymbol && gameState[4] === playerSymbol && gameState[7] === playerSymbol) ||
+   (gameState[2] === playerSymbol && gameState[5] === playerSymbol && gameState[8] === playerSymbol) ||
+   (gameState[6] === playerSymbol && gameState[7] === playerSymbol && gameState[8] === playerSymbol) ||
+   (gameState[0] === playerSymbol && gameState[4] === playerSymbol && gameState[8] === playerSymbol) ||
+   (gameState[2] === playerSymbol && gameState[4] === playerSymbol && gameState[6] === playerSymbol)
+   ){
+    return true;
+  }
+  else{
+   return false;
+ }
+}
+ 
 app.get('/api/play', (req, res) => {
-  res.send('');
-})
-
+ const {userId, position} = req.query;
+ if(parseInt(userId) === parseInt(playerTurn) && !gameState[position]) {
+   playerTurn = parseInt(userId) === 0 ? 1 : 0;
+   gameState[position] = players[userId].symbol;
+   if(checkWinner(gameState, players[userId].symbol)) {
+     res.json({winner:  players[userId].name});
+   } else {
+     res.json("ok");
+   }
+ 
+ } else {
+   res.status(400);
+   res.json('Invalid Move');
+ }
+ })
+ 
 app.get('/api/reset', (req, res) => {
-  res.send('');
+ gameState = ['', '', '', '', '', '', '', '', ''];
+ playerTurn = 0;
+ res.json(gameState);
 })
-
+ 
+app.get('/api/getPlayerInfo', (req, res) => {
+ if(!players[0]) {
+   players[0] = {
+     id: 0,
+     symbol: 'X',
+     name: "Player 1"
+   }
+   res.json(players[0]);
+ } else {
+   players[1] = {
+     id: 1,
+     symbol: '0',
+     name: "Player 2"
+   }
+   res.json(players[1]);
+ }
+ })
+ 
+ 
+wss.on('connection', (ws) => {
+ ws.on('message', (message) => {
+     if(message === 'Update') {
+       wss.clients.forEach(function each(client) {
+         client.send(JSON.stringify(gameState));
+       });
+     
+     }
+ });
+});
+ 
+ 
+ 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- src/App.js
+++ src/App.js
@@ -1,21 +1,80 @@
 import React from 'react';
 import './App.css';
-
-const App = () => {
-  const [gameState, setGameState] = React.useState([]);
-  const [playerId, setPlayerId] = React.useState(null);
-  const [winner, setWinner] = React.useState(null);
-
-  const reset = () => {
-  };
-
-  return (
-    <div className="App">
-      <button id="reset" onClick={reset}>Reset</button>
-      {winner && <h3 id="winner">Winner is {winner}</h3>}
-      Put game here!
-    </div>
-  );
+import axios from 'axios'
+const ws = new WebSocket('ws://localhost:4000')
+ 
+ws.onopen = () => {
+ console.log('connected')
 }
-
+ 
+const App = () => {
+ const [gameState, setGameState] = React.useState([]);
+ const [player, setPlayer] = React.useState(null);
+ const [winner, setWinner] = React.useState(null);
+ 
+ React.useEffect(() => {
+   setGameState(['', '', '', '', '', '', '', '', ''])
+   axios.get('/api/getPlayerInfo')
+   .then(res => {
+     setPlayer(res.data)
+   })
+   .catch(err => console.log(err));
+ }, []);
+ 
+ ws.onmessage = evt => {
+   const message = JSON.parse(evt.data)
+   setGameState(message)
+ }
+ 
+ const reset = () => {
+   axios.get(`/api/reset`)
+   .then(res => {
+     ws.send("Update")
+   })
+   .catch(err => {
+    console.log(err)
+   });
+ };
+ 
+ const handleClick = (e) => {
+   //server request
+   axios.get(`/api/play?userId=${player.id}&position=${parseInt(e.target.id.split('-')[1])}`)
+   .then(res => {
+     if(res.data.winner) {
+       setWinner(res.data.winner)
+     }
+     ws.send("Update")
+   })
+   .catch(err => {
+     if(err.response) {
+       window.alert(err.response.data);
+     }
+   });
+ }
+ 
+ return (
+   <div className="App">
+     <button id="reset" onClick={reset}>Reset</button>
+     {winner && <h3 id="winner">Winner is {winner}</h3>}
+     <div className="board">
+       <div className="row">      
+         <div onClick={handleClick} id="box-0" className="col"> {gameState[0]} </div>
+         <div onClick={handleClick} id="box-1" className="col"> {gameState[1]} </div>
+         <div onClick={handleClick} id="box-2" className="col"> {gameState[2]}</div>
+       </div>
+       <div className="row"> 
+         <div onClick={handleClick} id="box-3" className="col">  {gameState[3]} </div>
+         <div onClick={handleClick} id="box-4" className="col">  {gameState[4]} </div>
+         <div onClick={handleClick} id="box-5" className="col">  {gameState[5]} </div>
+       </div>
+       <div className="row"> 
+         <div onClick={handleClick} id="box-6" className="col">  {gameState[6]} </div>
+         <div onClick={handleClick} id="box-7" className="col">  {gameState[7]} </div>
+         <div onClick={handleClick} id="box-8" className="col">  {gameState[8]} </div>
+       </div>
+     </div>
+   </div>
+ );
+}
+ 
 export default App;
